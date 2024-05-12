37584f749320ba651d282665fd4e30d2d2724665
radmir <radmir@MSI>
2024-05-13 02:13:12 +0500

    addingTriangle

--- js/index.js
+++ js/index.js
@@ -1,60 +1,71 @@
  // создание поведения меню на телефон
-const menu = document.querySelector('.header__menu');
+ const menu = document.querySelector('.header__menu');
   
-const div = document.createElement('div');
-div.classList.add('header__burger');
+ const div = document.createElement('div');
+ div.classList.add('header__burger');
+ 
+ const label = document.createElement('label');
+ label.classList.add('nav__check')
+ 
+ for (let i = 0; i < 3; i+=1) {
+     const span = document.createElement('span');
+     label.appendChild(span);
+    
+ }
+ 
+ div.appendChild(label);
+ menu.appendChild(div);
+ 
+ 
+ const menuBody = document.querySelector('.menu__body');
+ 
+ div.addEventListener('click', () => {
+   if (menuBody.classList.contains('checked__burger')) {
+     menuBody.classList.remove('checked__burger');
+   } else {
+     menuBody.classList.add('checked__burger');
+   }
+ })
+ 
+ const upperSpan = document.querySelector('.nav__check span:first-child')
+ const middleSpan = document.querySelector('.nav__check span:nth-child(2)')
+ const buttonSpan = document.querySelector('.nav__check span:last-child')
+ 
+ label.addEventListener('click', () => {
+   if (menuBody.classList.contains('checked__burger')) {
+     upperSpan.classList.remove('upper__stick');
+     middleSpan.classList.remove('middle__stick');
+     buttonSpan.classList.remove('button__stick');
+   } else {
+     upperSpan.classList.add('upper__stick');
+     middleSpan.classList.add('middle__stick');
+     buttonSpan.classList.add('button__stick');
+   }
+ })
+ // создание треугольник 
+ const divTriangle = document.createElement('div');
+ divTriangle.classList.add('triangle');
+ const headerMenu = document.querySelector('.header__menu')
+ headerMenu.appendChild(divTriangle)
+ 
+  // поведение появления подменю
+ const menuList = document.querySelector('.menu__body ul');
+ menuList.classList.add('firstUl')
+ const bigOpt = document.querySelector('.firstUl > li:nth-of-type(3)');
+ const bigOptSub = document.querySelector('.menu__item:nth-of-type(3)> .menu__sub-list');
+ 
 
-const label = document.createElement('label');
-label.classList.add('nav__check')
+ const firstUlFourLi = document.querySelector('.firstUl > li:nth-of-type(4)');
+ const firstUlFifthLi = document.querySelector('.firstUl > li:nth-of-type(5)');
+ const firstUlSixthLi = document.querySelector('.firstUl > li:nth-of-type(6)');
+ 
+ divTriangle.addEventListener('click', () => {
+    bigOptSub.classList.toggle('visible__sub')
+    firstUlFourLi.classList.toggle('fourth__li__big');
+    firstUlFifthLi.classList.toggle('fifth__li__big');
+    firstUlSixthLi.classList.toggle('sixth__li__big');
 
-for (let i = 0; i < 3; i+=1) {
-    const span = document.createElement('span');
-    label.appendChild(span);
    
-}
+ })
 
-div.appendChild(label);
-menu.appendChild(div);
-
-
-const menuBody = document.querySelector('.menu__body');
-
-div.addEventListener('click', () => {
-  if (menuBody.classList.contains('checked__burger')) {
-    menuBody.classList.remove('checked__burger');
-  } else {
-    menuBody.classList.add('checked__burger');
-  }
-})
-
-const upperSpan = document.querySelector('.nav__check span:first-child')
-const middleSpan = document.querySelector('.nav__check span:nth-child(2)')
-const buttonSpan = document.querySelector('.nav__check span:last-child')
-
-label.addEventListener('click', () => {
-  if (menuBody.classList.contains('checked__burger')) {
-    upperSpan.classList.remove('upper__stick');
-    middleSpan.classList.remove('middle__stick');
-    buttonSpan.classList.remove('button__stick');
-  } else {
-    upperSpan.classList.add('upper__stick');
-    middleSpan.classList.add('middle__stick');
-    buttonSpan.classList.add('button__stick');
-  }
-})
-// создание треугольник 
-const divTriangle = document.createElement('div');
-divTriangle.classList.add('triangle');
-
- // поведение появления подменю
-const menuList = document.querySelector('.menu__body ul');
-menuList.classList.add('firstUl')
-const bigOpt = document.querySelector('.firstUl > li:nth-of-type(3)');
-const bigOptSub = document.querySelector('.menu__item:nth-of-type(3)> .menu__sub-list');
-bigOptSub.parentNode.insertBefore(divTriangle, bigOptSub);
-
-
-
-divTriangle.addEventListener('click', () => {
-  bigOptSub.classList.toggle('visible__sub')
-})
+ 
